6b148560f7e9b2ae63026ab20f33db1f8bd34991
Anagol <golosnoy69@gmail.com>
2020-06-19 14:05:54 +0300

    Commit_1

---
+++ testing.py
@@ -0,0 +1,8 @@
+import requests, bs4
+
+s = requests.get('https://play.tavr.media/radiorelax/int/')
+b = bs4.BeautifulSoup(s.text, 'html.parser')
+track_tag_singer = b.select('#singer0')
+track_tag_song = b.select('#song0')
+track = track_tag_singer[0].getText() + ' ' + track_tag_song[0].getText()
+print(track)
